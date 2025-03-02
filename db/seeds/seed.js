0df8bf223b503a90cb5bd015c6321647d1bfff7a
Rose Mullan <rfmullan@gmail.com>
2025-03-02 20:26:35 +0000

    complete seeding from previous example

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,50 +1,42 @@
-const db = require("../connection")
+const db = require('../connection.js');
 const format = require("pg-format")
+const {createRef, formatSnacksData} = require('./seed-utils.js')
 
-function seed({ snacksData, categoriesData, vendingMachineData }) {
-    return db.query(`DROP TABLE IF EXISTS snacks;`)
-        .then(() => { return db.query(`DROP TABLE IF EXISTS categories;`); })
-        .then(() => { return db.query(`DROP TABLE IF EXISTS vending_machines;`); })
-        .then(() => {
-            return db.query(`CREATE TABLE vending_machines (
-                vm_id SERIAL PRIMARY KEY,
-                vm_location VARCHAR(100),
-                vm_rating INT
-            );`);
+const seed = ({ categoriesData, snacksData }) => { 
+    return db.query('DROP TABLE IF EXISTS snacks;').then(() => { 
+        return db.query('DROP TABLE IF EXISTS categories')
+    }).then(() => { 
+        return db.query(`CREATE TABLE categories(
+            category_id SERIAL PRIMARY KEY,
+            category_name VARCHAR(40) NOT NULL)`)
+    }).then(() => { 
+        return db.query(`CREATE TABLE snacks(
+            snack_id SERIAL PRIMARY KEY,
+            snack_name VARCHAR(40) NOT NULL,
+            snack_description VARCHAR(100),
+            price_in_pence INT,
+            category_id INT REFERENCES categories(category_id))`
+        )
+    }).then(() => {
+        const formattedCategories = categoriesData.map((category) => { 
+            return [category.category_name]
         })
-        .then(() => {
-            return db.query(`CREATE TABLE categories(
-                category_id SERIAL PRIMARY KEY,
-                category_name VARCHAR(40) NOT NULL
-            );`);
-        })
-        .then(() => {
-            return db.query(`CREATE TABLE snacks(
-                snack_id SERIAL PRIMARY KEY,
-                snack_name VARCHAR(40) NOT NULL,
-                snack_description VARCHAR(100),
-                price_in_pence INT,
-                category_id INT,
-                FOREIGN KEY (category_id) REFERENCES categories(category_id)
-            );`);
-        })
-        .then(() => { 
-            const formattedCategories = categoriesData.map((category) => { 
-                return [category.category_name]
-            })
+        const insertCategoriesQueryString = format(`
+            INSERT INTO categories (category_name) VALUES %L RETURNING *;`,
+            formattedCategories)
+        
+        return db.query(insertCategoriesQueryString)
+        
+    }).then(({ rows }) => { 
 
-            const sqlString = format(`INSERT INTO categories (category_name)VALUES %L RETURNING *`, formattedCategories)
-            return db.query(sqlString)
-        })
-        .then(({rows}) => { 
-            console.log(rows)
+        const refObj = createRef(rows)
 
+        const formattedSnacks = formatSnacksData(snacksData, refObj)
 
-            // const formattedSnacks = snacksData.map((snack) => { 
-            //     return [snack.snack_name, snack.snack_description, snack.price_in_pence, ]
-            // })
-            // console.log(formattedSnacks)
-        })
-}
+        const queryStr = format(`INSERT INTO snacks (snack_name, snack_description, price_in_pence, category_id) VALUES %L`, formattedSnacks)
 
-module.exports = seed 
+        return db.query(queryStr)
+    })
+};
+
+module.exports = seed;
