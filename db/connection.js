77f32f3f2f4555b36695f6e35ae49fc44fe8dae2
Rose Mullan <rose.mullan@northcoders.com>
2025-06-11 09:06:38 +0100

    setup hosting

--- db/connection.js
+++ db/connection.js
@@ -1,16 +1,20 @@
 const { Pool } = require("pg");
 
-const pool = new Pool();
-
 const ENV = process.env.NODE_ENV || 'development'
 
 require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` })
 
-if (!process.env.PGDATABASE) {
-    throw new Error("no PGDATABASE set");
-} else { 
-    console.log("connected to:", process.env.PGDATABASE)
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+    throw new Error("PGDATABASE or DATABASE_URL not set");
+}
+
+const config = {};
+
+if (ENV === "production") {
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
 
-module.exports = pool
+
+module.exports = new Pool(config)
