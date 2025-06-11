662e17c7eb9d0f6c37ba96b10fc094b3c3eb1436
Rose Mullan <rose.mullan@northcoders.com>
2025-06-11 09:17:45 +0100

    move nodemon to dependencies

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,6 @@ require("dotenv").config({ path: `${__dirname}/../.env.${ENV}` })
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("PGDATABASE or DATABASE_URL not set");
 }
-
 const config = {};
 
 if (ENV === "production") {
